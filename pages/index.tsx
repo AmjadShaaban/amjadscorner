a2aa552fc81f1e6c917fe640b2d0ed2b3fb78561
Amjad Shaaban <theamjad@outlook.com>
2022-03-01 15:22:37 -0500

    Landing animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,112 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { Fade } from 'react-awesome-reveal';
-import Typewriter from 'typewriter-effect';
 import Button from 'react-bootstrap/Button';
 import { JackInTheBox } from 'react-awesome-reveal';
+import Particles from 'react-tsparticles';
+import Engine from 'react-tsparticles';
+import { FaEthereum, FaReact, FaBootstrap } from 'react-icons/fa';
+
+const particlesOptions: any = {
+  background: {
+    color: {
+      value: '#ffffff',
+    },
+  },
+  fpsLimit: 120,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: true,
+        mode: 'push',
+      },
+      onHover: {
+        enable: true,
+        mode: 'repulse',
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: '#000000',
+    },
+    links: {
+      color: '#000000',
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outMode: 'bounce',
+      random: false,
+      speed: 6,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      character: {
+        fill: true,
+        font: 'Font Awesome 5 Brands',
+        style: '',
+        value: () => FaReact,
+        weight: '400',
+      },
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+};
 
 const Home: NextPage = () => {
+  const particlesInit = (main) => {
+    console.log(main);
+  };
+
+  const particlesLoaded = (container: any) => {
+    console.log(container);
+  };
   return (
     <div className={styles.container}>
+      <Particles
+        id='tsparticles'
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={particlesOptions}
+      />
       <Head>
         <title>Amjad&apos;s Portfolio</title>
         <meta name='description' content="Amjad's Portfolio" />
@@ -24,20 +123,9 @@ const Home: NextPage = () => {
           </h1>
         </Fade>
 
-        <p className={styles.description}>
-          <p className={styles.code}>
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .typeString(
-                    'Full stack TypeScript/JavaScript Web/App Developer'
-                  )
-                  .changeDelay(20)
-                  .start();
-              }}
-            />
-          </p>
-        </p>
+        <Fade className={styles.description}>
+          <h3 className={styles.code}>Full-stack Developer</h3>
+        </Fade>
         <JackInTheBox delay={8000}>
           <Button variant='primary'>[PH]ENTER</Button>
         </JackInTheBox>
