780b33c73a860957f6b7297d6d2e0f13d2cdac07
Amjad Shaaban <amjad@valiancedev.com>
2025-04-04 13:05:24 -0400

    added role enum, added first and last names and user role to user model

--- models/auth/User.ts
+++ models/auth/User.ts
@@ -1,19 +1,35 @@
-import mongoose, { Document, Model, Schema } from 'mongoose';
-import {z} from 'zod';
+import { UserRole } from "@/types/roles";
+import mongoose, { Document, Model, Schema } from "mongoose";
+import { z } from "zod";
 
 const UserSchema = z.object({
-  email: z.string().email({message: 'Invalid email address'}),
-  password: z.string().min(8, {message: 'Password must be at least 8 characters long'}),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" }),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  role: z.nativeEnum(UserRole).default(UserRole.USER),
 });
 
 type IUser = z.infer<typeof UserSchema> & Document;
 
-const userSchema:Schema<IUser> = new Schema({
+const userSchema: Schema<IUser> = new Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  role: { type: String, enum: Object.values(UserRole), default: UserRole.USER },
 });
 
-const User:Model<IUser> = mongoose.models.User || mongoose.model<IUser>('User', userSchema);
+userSchema.set("toJSON", {
+  transform: (_doc, ret) => {
+    delete ret.password;
+    return ret;
+  },
+});
 
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", userSchema);
 
-export {User, UserSchema};
+export { User, UserSchema };
