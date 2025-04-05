ee0190fb7cab8c5308338c8697ca2e1112142801
Amjad Shaaban <amjad@valiancedev.com>
2025-04-05 10:54:38 -0400

    added QueryWrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { auth } from "@/lib/auth/auth";
 import Navbar from "@/components/Navbar";
 import SessionWrapper from "@/components/SessionWrapper";
+import QueryWrapper from "@/components/QueryWrapper";
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
@@ -12,12 +13,14 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
         <link rel="icon" href="/logo.png" />
       </head>
       <body className="min-h-screen bg-gradient-to-b from-gray-900 to-black">
-        <SessionWrapper initialSession={session}>
-          <div className="max-w-5xl mx-auto my-6 p-1 bg-gray-800 rounded-xl shadow-lg">
-            <Navbar />
-            <main>{children}</main>
-          </div>
-        </SessionWrapper>
+        <QueryWrapper>
+          <SessionWrapper initialSession={session}>
+            <div className="max-w-5xl mx-auto my-6 p-1 bg-gray-800 rounded-xl shadow-lg">
+              <Navbar />
+              <main>{children}</main>
+            </div>
+          </SessionWrapper>
+        </QueryWrapper>
       </body>
     </html>
   );
